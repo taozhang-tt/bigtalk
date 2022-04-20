0b59dd7ca69b7dc140ea27fa246cb4c7a598f49f
TT <taozhang.tt@gmail.com>
2022-04-20 14:13:24 +0800

    chore: 添加代理模式例子

--- chapter7/chapter7-3/main.go
+++ chapter7/chapter7-3/main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	main2()
+}
+
+func main1() {
 	jiaojiao := &SchoolGirl{"李娇娇"}
 	zhuojiayi := NewProxy(jiaojiao)
 
@@ -35,4 +39,52 @@ func (p *Proxy) GiveFlowers() {
 
 func (p *Proxy) GiveChocolate() {
 	fmt.Printf("%v 送你巧克力\n", p.mm.Name)
+}
+
+// 菜鸟教程的另一个例子
+type Image interface {
+	display()
+}
+
+type RealImage struct {
+	fileName string
+}
+
+func (ri *RealImage) display() {
+	fmt.Printf("Displaying: %v\n", ri.fileName)
+}
+
+func (ri *RealImage) loadFromDisk(fileName string) {
+	fmt.Printf("Loading: %v\n", fileName)
+}
+
+func NewRealImage(fileName string) *RealImage {
+	img := &RealImage{fileName}
+	img.loadFromDisk(fileName)
+	return img
+}
+
+type ProxyImage struct {
+	realImage *RealImage
+	fileName  string
+}
+
+func (pi *ProxyImage) display() {
+	if pi.realImage == nil {
+		pi.realImage = NewRealImage(pi.fileName)
+	}
+	pi.realImage.display()
+}
+
+func NewProxyImage(fileName string) *ProxyImage {
+	return &ProxyImage{fileName: fileName}
+}
+
+func main2() {
+	img := NewProxyImage("picture1.png")
+	// 图像将从磁盘加载
+	img.display()
+
+	// 图像将会直接加载
+	img.display()
 }
